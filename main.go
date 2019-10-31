3608aff8c58837c2796884ad1cefa2145cd4e318
Andras Mihaly <andrasm96@gmail.com>
2019-10-31 15:48:31 -0700

    Refactor two methods

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	printWelcome() 
-	printGreeting(getName()) //input getName() instead piping
-	fmt.Println("Let's go on an adventure!")
+	printGreeting(getName()) //input getName() instead piping, function is mselly if it does more than one thing. beyond it's defined capacity. 
+	fmt.Println("Let's go on an adventure!") //extract to a function perhaps
 	travel()
 }
 
@@ -28,11 +28,11 @@ func printGreeting(name string) {
 func travel() {
 	var choice string
 	for choice != "Y" && choice != "N" {
-		choice = getTravelChoice()
+		choice = getInput("Name the planet you would like to visit.")
 		if choice == "Y" {
 			travelToRandomPlanet()
 		} else if choice == "N" {
-			planetName := getPlanetName()
+			planetName := getInput("La di da")
 			travelToPlanet(planetName)
 		} else {
 			fmt.Println("Sorry, I didn't get that.")
@@ -40,11 +40,11 @@ func travel() {
 	}
 }
 
-func getTravelChoice() string {
-	var choice string
-	fmt.Println("Shall I randomly choose a planet for you to visit? (Y or N)")
-	fmt.Scan(&choice)
-	return choice
+func getInput(prompt string) string {
+	var response string
+	fmt.Println(prompt)
+	fmt.Scan(&response)
+	return response
 }
 
 func travelToRandomPlanet() {
@@ -52,12 +52,12 @@ func travelToRandomPlanet() {
 	fmt.Println("Arrived at Jupiter. The large red spot appears ominous.")
 }
 
-func getPlanetName() string {
-	var name string
-	fmt.Println("Name the planet you would like to visit.")
-	fmt.Scan(&name)
-	return name
-}
+// func getPlanetName() string {
+// 	var name string
+// 	fmt.Println("Name the planet you would like to visit.")
+// 	fmt.Scan(&name)
+// 	return name
+//}
 
 func travelToPlanet(planetName string) {
 	fmt.Printf("Traveling to %s...\n", planetName)

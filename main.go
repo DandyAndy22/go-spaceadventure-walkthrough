2c788009b3d3cd12bfa521983767d7d336434256
Andras Mihaly <andrasm96@gmail.com>
2019-11-04 19:59:55 -0800

    Add additional planet options

--- main.go
+++ main.go
@@ -29,8 +29,7 @@ func travel() {
 		if choice == "Y" {
 			travelToRandomPlanet()
 		} else if choice == "N" {
-			planetName := getInput("Name the planet you would like to visit.")
-			travelToPlanet(planetName)
+			travelToPlanet(getInput("Name the planet you would like to visit."))
 		} else {
 			fmt.Println("Sorry, I didn't get that.")
 		}
@@ -49,16 +48,36 @@ func travelToRandomPlanet() {
 	fmt.Println("Arrived at Jupiter. The large red spot appears ominous.")
 }
 
-// func getPlanetName() string {
-// 	var name string
-// 	fmt.Println("Name the planet you would like to visit.")
-// 	fmt.Scan(&name)
-// 	return name
-//}
-
 func travelToPlanet(planetName string) {
 	fmt.Printf("Traveling to %s...\n", planetName)
 		if planetName == "Neptune" {
 			fmt.Println("Arrived at Neptune. A very cold planet, furthest from the sun.")
+
+		}else if planetName == "Mars" {
+			fmt.Println("Arrived at the Mars, the red planet. Watch out for the locals!")
+
+		}else if planetName == "Venus" {
+			fmt.Println("Arrived at Venus, the other one near Earth. This place is wild.")
+
+		}else if planetName == "Mercury" {
+			fmt.Println("Arrived at Mercury. Where you're hot and you're cold and you're yes and you're no..")
+			
+		}else if planetName == "Jupiter" {
+			fmt.Println("Arrived at Jupiter. It's a real gas, man.")
+			
+		}else if planetName == "Uranus" {
+			fmt.Println("Arrived at Uranus. Nothing special, ooph.")
+			
+		}else if planetName == "Earth" {
+			fmt.Println("Arrived at Earth. The little blue dot, great surfing, cheese and tax shelters here.")
+			
+		}else if planetName == "Saturn" {
+			fmt.Println("Arrived at Saturn. Watch out, you're liable to get mooned out here.")
+			
+		}else if planetName == "Pluto" {
+			fmt.Println("Arrived at Pluto. Fake planet.")
+			
+		}else{
+			fmt.Println("Sorry, I didn't get that.")
 		}
 }

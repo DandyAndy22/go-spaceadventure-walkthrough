bf002b0d16ba24136fc8a2378a6ec2324e6de9f3
Andras Mihaly <andrasm96@gmail.com>
2019-11-12 20:30:19 -0800

    Add new main.go

---
+++ cmd/spaceadventure/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/dandyandy22/go-spaceadventure-walkthrough/internal/spaceadventure"
+
+func main(){
+	spaceadventure.Start(spaceadventure.PlanetarySystem{Name: "Solare System", Planets: []spaceadventure.Planet{}})
+}
+
+

71a7e26cc4973b9ff5f8631cddc99ecd74cdca4b
Andras Mihaly <andrasm96@gmail.com>
2019-11-12 20:22:59 -0800

    Add addtionally dimension to solar system; planets

--- main.go
+++
@@ -1,9 +0,0 @@
-package spaceadventure
-
-import "github.com/dandyandy22/go-spaceadventure-walkthrough/internal/spaceadventure"
-
-func main(){
-	spaceadventure.Start(spaceadventure.planetarySystem{"Solare System"})
-}
-
-
